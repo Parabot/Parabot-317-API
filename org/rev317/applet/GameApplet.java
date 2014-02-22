146368c42edf27c811e59b40e8dc6954d7a946ae
Parnassian <parnassian@pbot.org>
2014-02-22 17:34:44 +0100

    Revert paint injection and it's classes

--- org/rev317/applet/GameApplet.java
+++ org/rev317/applet/GameApplet.java
@@ -1,57 +1,24 @@
 package org.rev317.applet;
 
 import org.rev317.input.InputHandler;
-import org.rev317.painter.Painter;
 import org.rev317.script.ScriptEngine;
 
 import java.applet.Applet;
 import java.awt.*;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
-import java.awt.image.BufferedImage;
 
 /**
  *
  * @author Everel, Matt
  *
  */
-public abstract class GameApplet extends Applet implements Runnable{
+public abstract class GameApplet extends Applet implements Runnable {
     private static final long serialVersionUID = 1L;
     private ScriptEngine scriptEngine;
-    private Painter painter;
-    private boolean clientReady;
     
     public GameApplet() {
     	this.scriptEngine = ScriptEngine.getInstance();
-    	this.painter = Painter.getInstance();
-    }
-
-    @Override
-    public Graphics getGraphics() {
-        if(!clientReady)
-            return super.getGraphics();
-        if(painter == null) {
-            painter = Painter.getInstance();
-            return new BufferedImage(771, 531,
-                    BufferedImage.TYPE_INT_ARGB).createGraphics();
-        }
-        synchronized (painter.screen) {
-            return painter.game.createGraphics();
-        }
-    }
-
-    @Override
-    public void run(){
-        clientReady = true;
-        _run();
-    }
-
-    //Thanks to matt for the cache blackscreen fix
-
-    public abstract void _run(); // implement this shit in the hooks (you'll also need to remove the "Runnable" interface from the client's RSApplet)
-
-    public Graphics getRealGraphics() {
-        return super.getGraphics();
     }
 
     @Override
