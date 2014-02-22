61a22a37a93cb5df7e1a614bfd345bfd64234b80
Jeroen Ketelaar <jeroenketelaar@me.com>
2014-02-22 20:53:48 +0100

    Started on equipment debug

---
+++ org/rev317/debug/DEquipment.java
@@ -0,0 +1,27 @@
+package org.rev317.debug;
+
+import org.parabot.core.paint.AbstractDebugger;
+
+import java.awt.*;
+
+/**
+ * User: Jeroen
+ * Date: 22/02/14
+ * Time: 20:31
+ */
+public class DEquipment extends AbstractDebugger{
+    @Override
+    public void toggle() {
+
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+
+    @Override
+    public void paint(Graphics graphics) {
+
+    }
+}
