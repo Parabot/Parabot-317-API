ce0f1a0ffebc405d8a7f3b0616c29749ced3e868
demmonic <apocalypse9xx@hotmail.com>
2014-02-22 18:15:33 -0500

    finished equipment debugging

--- org/rev317/debug/DEquipment.java
+++ org/rev317/debug/DEquipment.java
@@ -1,27 +1,40 @@
 package org.rev317.debug;
 
 import org.parabot.core.paint.AbstractDebugger;
+import org.rev317.api.methods.Equipment;
+import org.rev317.api.wrappers.hud.Tab;
 
 import java.awt.*;
 
 /**
- * User: Jeroen
- * Date: 22/02/14
- * Time: 20:31
+ * 
+ * @author Paradox, Demmonic
+ *
  */
-public class DEquipment extends AbstractDebugger{
-    @Override
-    public void toggle() {
+class DEquipment extends AbstractDebugger {
+	
+	private boolean enabled = false;
+	
+	@Override
+	public void toggle() {
+		enabled = !enabled;
+	}
 
-    }
+	@Override
+	public boolean isEnabled() {
+		return enabled;
+	}
 
-    @Override
-    public boolean isEnabled() {
-        return false;
-    }
-
-    @Override
-    public void paint(Graphics graphics) {
-
-    }
+	@Override
+    public void paint(Graphics g) {
+		if (Tab.getOpened() != Tab.EQUIPMENT) {
+			return;
+		}
+		for (Equipment e : Equipment.values()) {
+			if (e.getEquippedId() != 0) {
+				g.drawString("" + e.getEquippedId(), e.getPoint().x, e.getPoint().y);
+			}
+		}
+	}
+    
 }
