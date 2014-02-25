7005c242ac62c34d0a788e7d5f948a42f1158cf1
Jeroen Ketelaar <jeroenketelaar@me.com>
2014-02-25 18:52:30 +0100

    Added login handler

--- org/rev317/randoms/Login.java
+++ org/rev317/randoms/Login.java
@@ -1,19 +1,21 @@
 package org.rev317.randoms;
 
+import org.parabot.core.ui.components.LogArea;
+import org.parabot.environment.api.utils.Time;
+import org.parabot.environment.input.Keyboard;
+import org.parabot.environment.input.Mouse;
 import org.parabot.environment.randoms.Random;
 import org.rev317.api.methods.Game;
+import org.rev317.randoms.ui.controllers.LoginController;
 import org.rev317.randoms.ui.controllers.UIController;
+import org.rev317.utils.Reader;
+
+import java.awt.*;
 
 /**
- * User: Jeroen
- * Date: 20/02/14
- * Time: 00:49
+ * Author: Sully
  */
 public class Login implements Random {
-
-    public static String username;
-    public static String password;
-
     @Override
     public boolean activate() {
         return !Game.isLoggedIn() && UIController.randoms.get("Auto login") == true;
@@ -21,7 +23,24 @@ public class Login implements Random {
 
     @Override
     public void execute() {
+        LogArea.log("Starting on login random");
+        //ToDo click cancel first
+        Point u = createPoint(Reader.readProvider("usernamePoint"));
+        Point p = createPoint(Reader.readProvider("passwordPoint"));
+        Point l = createPoint(Reader.readProvider("loginButtonPoint"));
+        Mouse.getInstance().click(u, true);
+        Time.sleep(2000);
+        Keyboard.getInstance().sendKeys(LoginController.getUsername());
+        Time.sleep(2000);
+        Mouse.getInstance().click(p, true);
+        Time.sleep(2000);
+        Keyboard.getInstance().sendKeys(LoginController.getPassword());
+        Time.sleep(2000);
+        Mouse.getInstance().click(l, true);
+        Time.sleep(2000);
+    }
 
+    public static Point createPoint(String s) {
+        return new Point(Integer.parseInt((s.split(","))[0]), Integer.parseInt((s.split(","))[1]));
     }
 }
-
